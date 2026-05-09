3fa6c8091f7fec9f62ae3ed2d77bd27345f85238
Victor Pellegrini <vpellegrini@furo.company>
2026-05-09 01:16:54 -0400

    fix: next.config rewrite syntax

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,20 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: 'noki.cl' }],
+          destination: '/noki/:path*',
+        },
+        {
+          source: '/',
+          has: [{ type: 'host', value: 'noki.cl' }],
+          destination: '/noki',
+        },
+      ],
+    }
+  },
 };
-
 export default nextConfig;
-
-async rewrites() {
-  return {
-    beforeFiles: [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'noki.cl' }],
-        destination: '/noki/:path*',
-      },
-      {
-        source: '/',
-        has: [{ type: 'host', value: 'noki.cl' }],
-        destination: '/noki',
-      },
-    ],
-  }
-},
